d4b23e96433c7d12b7c0b233d2ce3018905b2757
Guilherme <guilherme003souza@gmail.com>
2024-09-26 13:08:48 -0300

    Projeto em reta final

--- php/excluir_usuario_adm.php
+++ php/excluir_usuario_adm.php
@@ -1,6 +1,10 @@
 <?php
 require_once "conexao.php";
 require_once "functions.php";
+if(isset($_SESSION['id_adm'])){
 excluir_usuario_adm($conexao,$_SESSION['id_adm']);
 unset($_SESSION['id_adm']);
+}
+header("login_menuadm.php");
+
 ?>
--- php/menu_adm.php
+++ php/menu_adm.php
@@ -74,7 +74,7 @@
   </div>
   <script>
     function excluir_conta_adm(){
-      let confirmar  = confirm("Deseja mesmo excluir sua conta?");
+    let confirmar  = confirm("Deseja mesmo excluir seu usuário");
     if(confirmar === true){
         window.location.href = 'excluir_usuario_adm.php';
     }
